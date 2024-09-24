ded8cfadac6404ddc0ee33cc094ea1efff865bd3
Menna Amr <menaamr555@gmail.com>
2024-09-24 18:41:27 +0300

    feat:feature-create-page-schema-and-its-validation

--- config/db.js
+++ config/db.js
@@ -13,3 +13,21 @@ async function dbConection() {
 module.exports = {
   dbConection,
 };
+
+
+// db.js
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/Innovest', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected successfully');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
