58939f00d8cad46f2605786f26e02695a13a5059
Menna Amr <menaamr555@gmail.com>
2024-09-24 05:15:36 +0300

    [feat]:feature-create-notification-schema-and-validation

--- jest.config.js
+++ jest.config.js
@@ -2,3 +2,10 @@ module.exports = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['../server/tests/user.test.js'],
 };
+
+
+module.exports = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['../server/tests/notification.test.js'],
+};
+
