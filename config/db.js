e0afd5ef6c33c656910343eef89253a4cb257f1a
Menna Amr <menaamr555@gmail.com>
2024-09-27 09:26:05 +0300

    refactor : create-page-schema-and-its-validation

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 async function dbConection() {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // No need for deprecated options anymore
+    await mongoose.connect('mongodb://localhost:27017/Innovest'); // No need for deprecated options anymore
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
@@ -15,19 +15,4 @@ module.exports = {
 };
 
 
-// db.js
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/Innovest', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB connected successfully');
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
-    process.exit(1); // Exit process with failure
-  }
-};
-
-module.exports = connectDB;
